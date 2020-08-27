577000dc091c7dc4863c3f6eb9fb739815388786
Aleks Pickle <aleks.work2222@gmail.com>
2020-08-27 20:50:18 +0300

    fix main

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,23 @@ mod executor;
 pub mod types;
 use executor::{Executor, Game, Message};
 
-
 fn main() {
     let (tx, rx) = channel::<Arc<Mutex<Message>>>();
-    let mut game = Game::new();
-    let _ = Executor::new(rx, game).start();
-
-    let mut peter = game.add_player("Peter".into());
-    let mut hanna = game.add_player("Hanna".into());
+    let mut e = Executor::new(tx, rx, Game::new());
+    println!("Starting executor");
+    e.start();
+    
+    println!("Creating peter");
+    let peter = e.game.add_player("Peter".into());
+    println!("Creating hanna");
+    let hanna = e.game.add_player("Hanna".into());
 
     println!("Peter's position is: {:#?}", peter.position);
     println!("Hanna's life level is: {}", hanna.life);
-    
-    hanna.shout(Message::jump_left(peter), tx);
-    peter.shout(Message::eat_carrot(hanna), tx);
-    
+
+    e.shout(Message::jump_left(peter.clone()), &tx);
+    e.shout(Message::eat_carrot(hanna.clone()), &tx);
+
     println!("Peter's position is: {:#?}", peter.position);
     println!("Hanna's life level is: {}", hanna.life);
-
-
 }
